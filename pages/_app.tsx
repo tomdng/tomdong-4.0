3e03782e0b753e950eeffb436334c234afec6149
Tom Dong <contact@tomdong.io>
2021-03-22 00:34:01 -0500

    Cleaned up starter code and added tailwind to the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import type { AppProps } from "next/app";
+import React from "react"
+import type { AppProps } from "next/app"
 
-import "normalize.css/normalize.css";
-import "../styles/globals.css";
+import "tailwindcss/tailwind.css"
+import "normalize.css/normalize.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
