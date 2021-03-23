25b1ae67e1b1671b1a72752d64b61919561206d1
Tom Dong <contact@tomdong.io>
2021-03-23 00:13:08 -0500

    Added very raw content to the site (as Gordon Ramsay would say)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import type { AppProps } from "next/app"
 
 import "tailwindcss/tailwind.css"
-import "normalize.css/normalize.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
