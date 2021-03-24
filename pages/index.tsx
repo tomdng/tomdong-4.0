c91e7385dc9c9e67b614dcc7cf15065e0a0152fc
Tom Dong <contact@tomdong.io>
2021-03-23 23:03:30 -0500

    Added leaf

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,71 +10,79 @@ export default function Home() {
       </Head>
 
       <main>
-        <div>
-          <h1>
-            Tommy Dong <span>(yes that's my last name)</span>
-          </h1>
-          <p>
-            I'm a software engineer that builds <span>and designs</span> great
-            websites and meaningful applications for the web.
-          </p>
-          <ul>
-            <li>
-              <a href="#">GitHub</a>
-            </li>
-            <li>
-              <a href="#">Email</a>
-            </li>
-            <li>
-              <a href="#">Resume</a>
-            </li>
-          </ul>
-        </div>
+        <img
+          src="/leaf.svg"
+          alt="Logo"
+          className="text-green-500 dark:text-green-200"
+        />
 
         <div>
-          <p>Here are a few of my projects.</p>
           <div>
-            <p>
-              <a href="#">Propguard</a>
+            <h1 className="text-green-500">
+              Tommy Dong <span>(yes that's my last name)</span>
+            </h1>
+            <p className="text-purple-600">
+              I'm a software engineer that builds <span>and designs</span> great
+              websites and meaningful applications for the web.
             </p>
-            <p>
-              Codecademy-style interactive learning tool built with React and
-              Gatsby that teaches college students how to write autonomous
-              flight code for quadcopters.
-            </p>
-            <p>
-              It also comes with a visual tool to create/modify custom tutorials
-              so the user experience is good all around.
-            </p>
+            <ul>
+              <li>
+                <a href="#">GitHub</a>
+              </li>
+              <li>
+                <a href="#">Email</a>
+              </li>
+              <li>
+                <a href="#">Resume</a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <p>Here are a few of my projects.</p>
+            <div>
+              <p>
+                <a href="#">Propguard</a>
+              </p>
+              <p>
+                Codecademy-style interactive learning tool built with React and
+                Gatsby that teaches college students how to write autonomous
+                flight code for quadcopters.
+              </p>
+              <p>
+                It also comes with a visual tool to create/modify custom
+                tutorials so the user experience is good all around.
+              </p>
+            </div>
+
+            <div>
+              <p>
+                <a href="#">China King</a> and <a href="#">Sushi Ai</a>
+              </p>
+              <p>
+                There was a Chinese restaurant and a Japanese sushi place that
+                didn’t have good websites (and therefore no internet traffic).
+                So I went ahead and built two new websites with React and Gatsby
+                that not only looked better, but loaded much faster.
+              </p>
+              <p>
+                As an added bonus the improved SEO netted several thousand more
+                page views.
+              </p>
+            </div>
           </div>
 
           <div>
             <p>
-              <a href="#">China King</a> and <a href="#">Sushi Ai</a>
+              I love using JavaScript <span>(TypeScript)</span> and Python, but
+              I'm also venturing out and learning Go for a fresh perspective.
             </p>
+
             <p>
-              There was a Chinese restaurant and a Japanese sushi place that
-              didn’t have good websites (and therefore no internet traffic). So
-              I went ahead and built two new websites with React and Gatsby that
-              not only looked better, but loaded much faster.
-            </p>
-            <p>
-              As an added bonus the improved SEO netted several thousand more
-              page views.
+              When I’m not working I like reading, taking photos/videos, and
+              leading a <a href="#">onderful drone engineering team.</a>
             </p>
           </div>
-        </div>
-
-        <div>
-          <p>
-            I love using JavaScript <span>(TypeScript)</span> and Python, but
-            I'm also venturing out and learning Go for a fresh perspective.
-          </p>
-
-          <p>
-            When I’m not working I like reading, taking photos/videos, and
-            leading a <a href="#">onderful drone engineering team.</a>
-          </p>
         </div>
       </main>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'media', // or 'media' or 'class'
+  // darkMode: "media", // or 'media' or 'class'
   theme: {
+    colors: {},
+    // fontFamily: {
+    //   sans: ["Inter", "sans-serif"],
+    //   serif: ["Source Sans Pro", "serif"],
+    // },
     extend: {},
   },
   variants: {
